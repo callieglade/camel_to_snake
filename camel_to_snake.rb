392fc0ade29944ea6a40c12e97f1007adf013327
Callie Glade <callie.glade@meraki.net>
2020-02-25 13:33:01 -0800

    Refactor to read in files more efficiently by line

--- camel_to_snake.rb
+++ camel_to_snake.rb
@@ -1,17 +1,14 @@
 # guess who wrote 900 lines of ruby in camelCase instead of snake_case lol
 
 def camel_to_snake(file_name)
-  file_open = File.open(file_name)
-  file = file_open.readlines
-  file.each do |line|
+  f = File.new(file_name.gsub(".", "_snaked."), "w+")
+  File.foreach(file_name) do |line|
     if line[/\W([a-z]+[A-Z]\w*)/, 1].nil? == false
       line.gsub!(/\W([a-z]+[A-Z]\w*)/) do |var|
         var.gsub!(/[A-Z]/) {|m| "_" + m.downcase}
       end
     end
+    f.write line
   end
-  output = file.join
-  new_file = file_name.gsub(".", "_modified.")
-  File.open(new_file, "w") {|f| f.write output}
-  file_open.close
+  f.close
 end
