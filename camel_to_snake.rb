4c443fd0bc0a24d16ba8110e027e15c109024d15
Callie Glade <callie.glade@meraki.net>
2020-02-24 17:32:29 -0800

    Put each matching line and its index in a hash in an array 'matches'

--- camel_to_snake.rb
+++ camel_to_snake.rb
@@ -1,9 +1,10 @@
 # guess who wrote 900 lines of ruby in camelCase instead of snake_case lol
+# Regex Magic: /\W([a-z]+[A-Z]\w*)/
 
 def camel_to_snake(file_name)
   file_open = File.open(file_name)
   file = file_open.readlines.map(&:chomp)
-  file.each do |line|
-    #
-  end
+  content = file.select{|line| line[/\W([a-z]+[A-Z]\w*)/, 1].nil? == false}
+  index = content.map{|line| file.index(line)}
+  matches = Array.new(content.length) {|i| {content: content[i], index: index[i]}}
 end
