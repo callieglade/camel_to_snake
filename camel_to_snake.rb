dfb62249bdbc33f41ea7b44458888aaa07f2d9d3
Callie Glade <callie.glade@meraki.net>
2020-02-24 19:33:23 -0800

    Adjust methods to use gsub instead of select/map/etc

--- camel_to_snake.rb
+++ camel_to_snake.rb
@@ -1,10 +1,16 @@
 # guess who wrote 900 lines of ruby in camelCase instead of snake_case lol
-# Regex Magic: /\W([a-z]+[A-Z]\w*)/
 
 def camel_to_snake(file_name)
   file_open = File.open(file_name)
-  file = file_open.readlines.map(&:chomp)
-  content = file.select{|line| line[/\W([a-z]+[A-Z]\w*)/, 1].nil? == false}
-  index = content.map{|line| file.index(line)}
-  matches = Array.new(content.length) {|i| {content: content[i], index: index[i]}}
+  file = file_open.readlines
+  file.each do |line|
+    if line[/\W([a-z]+[A-Z]\w*)/, 1].nil? == false
+      line.gsub(/\W([a-z]+[A-Z]\w*)/) do |var|
+        var.gsub(/[A-Z]/) {|m| "_" + m.downcase}
+      end
+    end
+  end
+  output = file.join
+  file_name.gsub(".", "_modified.")
+  File.write(file_name, output)
 end
