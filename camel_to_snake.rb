c82066c771d82dc7af06a60251e787ea457bc15b
Callie Glade <callie.glade@meraki.net>
2020-02-25 13:00:16 -0800

    Change #gsub to #gsub! and properly close the opened file

--- camel_to_snake.rb
+++ camel_to_snake.rb
@@ -5,12 +5,13 @@ def camel_to_snake(file_name)
   file = file_open.readlines
   file.each do |line|
     if line[/\W([a-z]+[A-Z]\w*)/, 1].nil? == false
-      line.gsub(/\W([a-z]+[A-Z]\w*)/) do |var|
-        var.gsub(/[A-Z]/) {|m| "_" + m.downcase}
+      line.gsub!(/\W([a-z]+[A-Z]\w*)/) do |var|
+        var.gsub!(/[A-Z]/) {|m| "_" + m.downcase}
       end
     end
   end
   output = file.join
   new_file = file_name.gsub(".", "_modified.")
   File.open(new_file, "w") {|f| f.write output}
+  file_open.close
 end
